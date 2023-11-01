bfd3f8763f69fa43b3639c611f634a161ffec1be
ulan atabekov <45645644z@gmail.com>
2023-11-01 20:40:22 +0600

    Finished task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const PrototypeForm = {
+        firstName: 'Barbara',
+        lastName: 'Hepworth',
+        email: 'bhepworth@react.com',
+        hobby: 'love cats'
+    }
+
+    const [form, setForm] = useState(PrototypeForm);
+
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '250px',
+        margin: 'auto',
+        marginTop: '50px',
+        background: '#ccc',
+        padding: '0 5px',
+    };
+
+    const inputStyle = {
+        borderRadius: '5px',
+        marginBottom: '5px',
+    };
+
+    const textStyle = {
+        margin: '5px'
+    }
+
+
+    return (
+        <>
+            <div className="app" style={containerStyle}>
+                <label htmlFor='fname'>First Name:</label>
+                <input type="text"
+                       style={inputStyle}
+                       id='fname'
+                       placeholder='Enter name'
+                       value={form.firstName}
+                       onChange={e => {setForm({...form,
+                           firstName: e.target.value})}}/>
+                <label htmlFor="lname">Last Name: </label>
+                <input type="text"
+                       id='lname'
+                       style={inputStyle}
+                       placeholder='Enter last name'
+                       value={form.lastName}
+                       onChange={e => {setForm({...form,
+                           lastName: e.target.value})}}/>
+                <label htmlFor="email">Email: </label>
+                <input type="text"
+                       id='email'
+                       style={inputStyle}
+                       value={form.email}
+                       placeholder='Enter email'
+                       onChange={e => {setForm({...form,
+                           email: e.target.value})}}/>
+                <label htmlFor="hobby">Telephone: </label>
+                <input type="text"
+                       id='hobby'
+                       style={inputStyle}
+                       value={form.hobby}
+                       placeholder='Enter your hobby'
+                       onChange={e => {setForm({...form,
+                           hobby: e.target.value})}}/>
+                <hr/>
+
+                <p style={textStyle}>First name:{form.firstName}</p>
+                <p style={textStyle}>Last name:{form.lastName}</p>
+                <p style={textStyle}>Email: {form.email}</p>
+                <p style={textStyle}>Hobby: {form.hobby}</p>
+            </div>
+        </>
+
+    );
+};
 
 export default App;
